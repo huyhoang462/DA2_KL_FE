3af4846f172331a1625f48f9ce104f1a688a0608
huyhoang462 <22520462@gm.uit.edu.vn>
2026-01-03 16:23:55 +0700

    chore: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: [
+        '__vite-optional-peer-dep:@solana-program/system:@privy-io/react-auth:false',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '__vite-optional-peer-dep:@solana-program/system:@privy-io/react-auth:false':
+        () => null,
+    },
+  },
 });
