a44240578bd57597134b1b30aa4b6459267f83e0
dohuy0708 <dohuy22314@gmail.com>
2026-06-09 23:22:24 +0700

    feat; claim fund

--- src/constants/web3.js
+++ src/constants/web3.js
@@ -6,12 +6,12 @@ import MockUSDTArtifact from '../abis/MockUSDT.json';
 // Địa chỉ Smart Contract của bạn (có thể override bằng env)
 // cu export const CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
-export const CONTRACT_ADDRESS = '0xEA4a166beEB0db64C8f9d86Dd61bB9CC5Ca5F978';
+export const CONTRACT_ADDRESS = '0x6EcdAB152dF7bACE887FA6382259DADe3Aa760a8';
 
 export const USDT_TOKEN_ADDRESS = '0xB93e8F4905C889DDB24D0FD13ffa81668f7A26a2';
 
 // Bổ sung địa chỉ Marketplace (Cập nhật sau khi deploy)
-export const MARKETPLACE_ADDRESS = '0xBa076836D6Ad7fc9A8dD83d7366fD52AA48395b8';
+export const MARKETPLACE_ADDRESS = '0x19357De551cc161c801c207641f40B5cb36C4499';
 
 // Bóc tách đúng cái mảng "abi" từ trong file JSON ra
 export const CONTRACT_ABI = ShineTicketArtifact.abi;
