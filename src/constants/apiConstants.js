8a5aaaaaf369586f858cfd2ef6142a4fcd1fc298
huyhoang462 <22520462@gm.uit.edu.vn>
2026-06-20 09:23:46 +0700

    update: vite env variable

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,4 +1,5 @@
 //export const API_BASE_URL = 'https://da2-kl-be.onrender.com/api';
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/api';
-export const SERVER_URL = import.meta.env.SERVER_URL || 'http://localhost:3001';
+export const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
